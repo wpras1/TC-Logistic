24a7fe02cfb62bca64669dbf8163687401e5e477
wpras1 <wahyupras.work@gmail.com>
2025-01-08 16:36:08 +0700

    Update Title Web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard - Inventory</title>
+    <title>Tech Logistic</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
